01026cd7efe8afb5754bf01b1ebaa36ecf393926
Cyrille Favreau <cyrille.favreau@epfl.ch>
2016-05-08 09:55:41 +0900

    Upgraded to OSPRay 0.10 (Devel)

--- scripts/release_macosx.sh
+++ scripts/release_macosx.sh
@@ -22,6 +22,13 @@ export LIBRARY_PATH=
 export DYLD_LIBRARY_PATH=
 TBB_PATH_LOCAL=$PWD/tbb
 
+umask=`umask`
+function onexit {
+  umask $umask
+}
+trap onexit EXIT
+umask 002
+
 mkdir -p build_release
 cd build_release
 # make sure to use default settings
@@ -56,8 +63,8 @@ cmake \
 -D CMAKE_INSTALL_LIBDIR=lib \
 -D CMAKE_INSTALL_DOCDIR=doc \
 -D CMAKE_INSTALL_BINDIR=bin \
+-D TBB_ROOT=$TBB_PATH_LOCAL \
 ..
-#-D TBB_ROOT=$TBB_PATH_LOCAL \
 
 # create ZIP files
 make -j 4 package
